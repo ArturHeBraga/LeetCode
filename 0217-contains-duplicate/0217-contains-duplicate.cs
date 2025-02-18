9155e82922a7355f1aaed04a9470cdca7cc232c5
Artur Braga <68078436+ArturHeBraga@users.noreply.github.com>
2025-02-18 04:28:41 -0300

    Time: 85 ms (9.43%), Space: 57.6 MB (77.07%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cs
+++ 0217-contains-duplicate/0217-contains-duplicate.cs
@@ -1,6 +1,6 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        //Variable declaring
+        //Organizing the array so we can do just one loop
         Array.Sort(nums);
         //Loop to run the array
         for (int i = 0; i < nums.Length-1; i++) {
